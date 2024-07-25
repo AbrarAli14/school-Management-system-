15960907ef6fe1826345450a824b154674f27945
AbrarAli14 <AlFati4560@gmail.com>
2024-07-24 19:02:41 -0900

    commits

---
+++ resources/views/partials/inc_top.blade.php
@@ -0,0 +1,39 @@
+<link rel="icon" href="{{ asset('global_assets/images/favicon.png') }}">
+
+{{--<!-- Global stylesheets -->--}}
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js" integrity="sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href="{{ asset('global_assets/css/icons/icomoon/styles.css') }}" rel="stylesheet" type="text/css">
+    <link href=" {{ asset('assets/css/bootstrap_limitless.min.css') }}" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href=" {{ asset('assets/css/layout.min.css') }}" rel="stylesheet" type="text/css">
+    <link href=" {{ asset('assets/css/components.min.css') }}" rel="stylesheet" type="text/css">
+    <link href=" {{ asset('assets/css/colors.min.css') }}" rel="stylesheet" type="text/css">
+    <!-- /global stylesheets -->
+
+{{--DatePickers--}}
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datepicker.min.css') }}" type="text/css">
+
+{{-- Custom App CSS--}}
+<link href=" {{ asset('assets/css/qs.css') }}" rel="stylesheet" type="text/css">
+
+{{--   Core JS files --}}
+    <script src="{{ asset('global_assets/js/main/jquery.min.js') }} "></script>
+    <script src="{{ asset('global_assets/js/main/bootstrap.bundle.min.js') }} "></script>
+    <script src="{{ asset('global_assets/js/plugins/loaders/blockui.min.js') }} "></script>
+<style>
+ @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&family=Roboto:wght@300;400;500;700;900&display=swap');
+
+
+ html {
+      font-family: 'Quicksand', sans-serif !important;
+      font-weight:700;
+ }
+ body {
+      font-family: 'Quicksand', sans-serif !important;
+            font-weight:700;
+
+ }
+</style>
